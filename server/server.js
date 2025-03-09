f4a418fbecfea0e591a5f6b42a2b130d80317f2d
Kelly Hookham <hookhamk8@gmail.com>
2025-03-09 12:36:31 -0500

    route fix

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Middleware
 app.use(bodyParser.json());
 
 // Routes
-app.get('/', (_req, res) => {
-    res.send('Welcome to the Book Search API');
-});
+
+    app.get('*', (_req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
 
 // Start the server
 app.listen(PORT, () => {
